f2e32b4b7932247cd9393afe200fe3b28ebe0068
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-12-11 16:45:08 +0700

    .
    .

--- n.05-docker-mongo/src/models/index.js
+++ n.05-docker-mongo/src/models/index.js
@@ -1,7 +1,7 @@
 import User from "./User.js";
 import Tour from "./Tours.js";
 import Ticket from "./Tickets.js"
-import Booking from "./booking.js";
+import Booking from "./Booking.js";
 import Payment from "./Payment.js";
 import Flight from "./Flight.js";
 export{
